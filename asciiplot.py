24036744d37eb9f7c7b4f54cdb8ec64c962f26a3
Chandler Watson <WatsonC@MacBook13-494.local>
2013-11-03 15:50:36 -0800

    Fix scale support

--- asciiplot.py
+++ asciiplot.py
@@ -3,38 +3,46 @@ import math
 def normal(std_dev, mean, x):
 	return 1.0/(std_dev*math.sqrt(2.0*math.pi))*math.pow(math.e, -0.5*pow(x-mean, 2.0)/pow(std_dev, 2.0))
 
-def plot_points(points, blank=" ", filled="x", width=30, height=50):
-	xmin, xmax = min(point[0] for point in points), max(point[0] for point in points)
-	ymin, ymax = min(point[1] for point in points), max(point[1] for point in points)
+def plot_points(points, xmin=-10, xmax=10, ymin=-10, ymax=10, width=150, height=40, blank=" ", filled="X", pointlist=False, axis=True):
 	scale = [width/(xmax-xmin), height/(ymax-ymin)]	
 	xming, yming = scale[0]*xmax, scale[1]*ymax
 	grid = [[blank for x in range(width+1)] for y in range(height+1)]
+	
+	if ymin < 0 and  ymax > 0:
+		for x in range(0, len(grid[0])):
+			grid[int(round(-ymin*scale[1]))][x] = "-"
 
-	#PS
-	# -5, -5 - xmin, -ymin	
-
+	if xmin < 0 and xmax > 0:
+		for y in range(0, len(grid)):
+			grid[y][int(round(-xmin*scale[0]))] = "|"
+		
 	for point in points:
-		dx, dy = point[0] + xmin, point[1] + ymin 
-		xgrid, ygrid = int(round(dx*scale[0])), int(round(dy*scale[1]))			
-		grid[ygrid][xgrid] = filled
-	grid.reverse()
+		print point[0], point[1]
+		newx, newy = point[0] - xmin, point[1] - ymin # shift over 
+		xgrid, ygrid = int(round(newx*scale[0])), int(round(newy*scale[1])) # and scale!			
+		if 0 <= xgrid and xgrid <= width and 0 <= ygrid and ygrid <= height:
+			grid[ygrid][xgrid] = filled
 
+	grid.reverse()
 	for row in grid:
 		print "".join(row) 
 
 
-def plot_eq(eq, start, end, step=1.0):
+def plot_eq(eq, xmin=-10, xmax=10, ymin=-10, ymax=10, step=1.0, width=150, height=40, blank=" ", filled="X", pointlist=False, axis=True): #make y min/max value work!
 	points = []
-	x = start
-	while x <= end:
+	x = xmin
+	while x <= xmax:
 		points.append([float(x), float(eq(x))])
 		x += step
-	
-	plot_points(points)
+
+	plot_points(points, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, width=width, height=height, blank=blank, filled=filled, pointlist=False, axis=axis)
 
 def parabola(x):
 	return math.pow(x, 2)
 
-plot_eq(parabola, -5, 5, 0.2)
+def line(x):
+	return 5-x
+
+plot_eq(parabola, step=0.05, pointlist=True) # make axis not show if xmax > 0 or ymax > 0, etc....
 
 
